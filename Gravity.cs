0ef5bd1eb0e1b3fee0822e95f17013771dffab4a
BenM77 <34111213+BenM77@users.noreply.github.com>
2019-03-26 13:55:07 -0700

    This is the completed project
    Upload the relevant files since we can't just upload the whole Unity project

---
+++ Gravity.cs
@@ -0,0 +1,28 @@
+﻿//Ben Michael and Tristan Hildahl
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+
+    float deviceOrientX;
+    float deviceOrientY;
+    Gyroscope theGyro;
+    //Rigidbody2D sandRB2D;
+    void Start ()
+    {
+        theGyro = Input.gyro;
+        theGyro.enabled = true;
+        //sandRB2D = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate()
+    {
+        deviceOrientX = theGyro.attitude.x;
+        deviceOrientY = theGyro.attitude.y;
+
+        Physics2D.gravity = new Vector2(deviceOrientX, deviceOrientY);
+        //sandRB2D.AddForce(new Vector2(deviceOrientX, deviceOrientY));
+    }
+}
---
+++ ParticleSelector.cs
@@ -0,0 +1,49 @@
+﻿//Ben Michael and Tristan Hildahl
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSelector : MonoBehaviour
+{
+    enum particleType { Sand, Water, Mud} // could just use an int, but this makes things clear
+    public static int particleTypeNum;
+    public GameObject sandSelector;
+    public GameObject waterSelector;
+    public GameObject mudSelector;
+    GameObject[] selectors;
+
+    void Start()
+    {
+        selectors = new GameObject[] { sandSelector, waterSelector, mudSelector};
+    }
+
+    void ActivateSelector(GameObject activeSelector)//this functions is for expandability purposes and simplifies the below functions
+    {
+        for(int i = 0; i < selectors.Length; i++)
+        {
+            GameObject curSelector = selectors[i];
+            if (curSelector == activeSelector)
+                curSelector.SetActive(true);
+            else
+                curSelector.SetActive(false);
+        }
+    }
+
+    public void setSand()//these functions should be consolidated if more particles added in future
+    {
+        particleTypeNum = (int)particleType.Sand;
+        ActivateSelector(sandSelector);
+    }
+
+    public void setWater()
+    {
+        particleTypeNum = (int)particleType.Water;
+        ActivateSelector(waterSelector);
+    }
+
+    public void setMud()
+    {
+        particleTypeNum = (int)particleType.Mud;
+        ActivateSelector(mudSelector);
+    }
+}
