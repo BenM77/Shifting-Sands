695b95c493703cde2bddd1918699e2b33814c561
BAM-LAPTOP77\Ben <michaben@live.com>
2019-03-26 15:27:05 -0700

    Added the rest of the Unity files for the complete project

---
+++ Assets/Scripts/SandCollisionController.cs
@@ -0,0 +1,20 @@
+﻿//Ben Michael and Tristan Hildahl
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SandCollisionController : MonoBehaviour
+{
+    public GameObject Mud;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Water")
+        {
+            Vector3 newPosition = this.transform.position;
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+            Instantiate(Mud, newPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,36 @@
+﻿//Ben Michael and Tristan Hildahl
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnScript : MonoBehaviour
+{
+
+    public GameObject Sand;
+    public GameObject Water;
+    public GameObject Mud;
+    Vector3 originPosition;
+
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled)//true while the touch is in any of the active phases
+            {
+                originPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 5));//to translate from screen position to world position
+                switch (ParticleSelector.particleTypeNum)
+                {
+                    case 0:
+                        Instantiate(Sand, originPosition, Quaternion.identity);//identity quaternion used for default rotation
+                        break;
+                    case 1:
+                        Instantiate(Water, originPosition, Quaternion.identity);
+                        break;
+                    case 2:
+                        Instantiate(Mud, originPosition, Quaternion.identity);
+                        break;
+                }
+            }
+        }
+    }
+}
